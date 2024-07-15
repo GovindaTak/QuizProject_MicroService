d874deaf118bb59bada8c31d5ef026d438eaa9cc
govinda tak <147643627+GovindaTak@users.noreply.github.com>
2024-07-15 11:19:51 +0530

    initial commit

---
+++ backend/Question_Service/src/main/java/com/question/Question_Service/model/Response.java
@@ -0,0 +1,11 @@
+package com.question.Question_Service.model;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class Response {
+    private Integer id;
+    private String response;
+}
